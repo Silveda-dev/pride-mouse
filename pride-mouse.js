1c2fb790253838c9741c937e7bde7c1b81ce42bf
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2024-02-15 18:18:17 +1030

    Add lesbian flags on mouse movement

--- pride-mouse.js
+++ pride-mouse.js
@@ -1,9 +1,20 @@
 let x = 0;
 let y = 0;
+let flags = [];
 
 addEventListener("mousemove", (event) => {
     x = event.offsetX;
     y = event.offsetY;
     console.log(x);
     console.log(y);
+    spawnFlag(x,y);
 });
+
+function spawnFlag(mouse_x,mouse_y) {
+    let flag = new Image(20,10);
+    flag.src = "lesbian-48.svg";
+    flag.top = 5 + mouse_y + "px";
+    flag.left = 2 + mouse_x + "px";
+    flags.push(flag);
+    document.body.appendChild(flag);
+}
