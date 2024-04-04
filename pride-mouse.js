c514631b37ebd74caccfcf26cc1f907c4ef68687
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2024-04-04 22:39:03 +1030

    Test single flag following mouse

--- pride-mouse.js
+++ pride-mouse.js
@@ -1,11 +1,22 @@
 //Pride mouse trail code
 
-//Testing tracking mouse coordinates
+//Initialising image
+document.body.id = "bod";
+var flag = document.createElement("img");
+flag.src = "lesbian.svg";
+flag.style.height = "36px";
+flag.style.width = "24px";
+flag.style.position = "absolute";
 
-document.onmousemove = (event) => {
-    const {
-        clientX,
-        clientY
-    } = event;
-    console.log("Mouse position: ", clientX, clientY);
+//Track mouse coordinates
+document.addEventListener("mousemove", update_mouse);
+var mouse_x, mouse_y;
+
+function update_mouse(e) {       
+    mouse_x = e.clientX;
+    mouse_y = e.clientY;
+    console.log("Mouse position: ", mouse_x, mouse_y);
+    flag.style.left = mouse_x + "px";
+    flag.style.top = mouse_y + "px";
+    document.getElementById("bod").appendChild(flag);
 };
